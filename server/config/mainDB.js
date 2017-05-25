1185854c8d5a405ea2f297617ef1475ee4a4cb31
systemis <systemofpeter@gmail.com>
2017-05-25 13:03:44 +0700

    eidt method in database

--- server/config/mainDB.js
+++ server/config/mainDB.js
@@ -2,33 +2,13 @@ var mysql = require('mysql');
 
 module.exports = mysql.createConnection({
     connectionLimit: 100,
-    host: 'sql12.freemysqlhosting.net',
-    port: 3306,
-    user: 'sql12176052',
-    password: 'JqdQz3VKBC',
-    database: 'sql12176052',
-    //socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'TruongThinhECommerce',
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
-
-// module.exports = mysql.createConnection({
-//     connectionLimit: 100,
-//     host: 'mysql.hostinger.vn',
-//     port: 3306,
-//     user: 'u446271841_ttdat',
-//     password: 'dOynnY1713C',
-//     database: 'u446271841_ttdat',
-//     //socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-// })
-
-// module.exports = mysql.createConnection({
-//     connectionLimit: 100,
-//     host: 'localhost',
-//     port: 8889,
-//     user: 'root',
-//     password: 'root',
-//     database: 'TruongThinhECommerce',
-//     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-// })
 
 /**
  * 
