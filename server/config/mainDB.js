d14552a7347a91faa786205a91d12e470caae74f
systemis <systemofpeter@gmail.com>
2017-06-09 16:23:10 +0700

    statuss item ui

--- server/config/mainDB.js
+++ server/config/mainDB.js
@@ -1,34 +1,5 @@
 var mysql = require('mysql');
 
-// module.exports = mysql.createConnection({
-//     connectionLimit: 100,
-//     host: 'mysql7.000webhosting.com',
-//     // port: 3306,
-//     user: 'id561439_ttprojectdata',
-//     password: 'Technology12',
-//     database: '	id561439_ttprojectdata',
-// })
-
-
-// module.exports = mysql.createConnection({
-//     connectionLimit: 100,
-//     host: 'sql12.freemysqlhosting.net',
-//     port: 3306,
-//     user: 'sql12176052',
-//     password: 'YES',
-//     database: 'sql12176052',
-// })
-
-// module.exports = mysql.createConnection({
-//     connectionLimit: 100,
-//     host: 'mysql.hostinger.vn',
-//     port: 3306,
-//     user: 'u446271841_ttapp',
-//     password: 'Technology12',
-//     database: '	u446271841_ttapp',
-// })
-
-
 module.exports = mysql.createConnection({
     connectionLimit: 100,
     host: 'localhost',
@@ -38,31 +9,3 @@ module.exports = mysql.createConnection({
     database: 'TruongThinhECommerce',
     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
-
-/**
- * 
- * user:u446271841_
- * Database: u446271841_ truongthinhdatabase
- * MK: e1RKlWEOiGGwr
- * 
- */
-
-// var mysql = require('mysql');
-
-// module.exports = mysql.createConnection({
-//     connectionLimit: 100,
-//     host: 'mysql.hostinger.vn',
-//     user: 'u446271841_ttdat',
-//     password: 'WL2p5mTG0yhX',
-//     database: 'u446271841_ttdat',
-//     //port: 3306,
-//     //socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-// })
-
-// // /**
-// //  * 
-// //  * user:u446271841_ttdat
-// //  * Database: u446271841_ttdat
-// //  * MK: e1RKlWEOiGGwr
-// //  * WL2p5mTG0yhX
-// //  */
