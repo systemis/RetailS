128f1bf9aed16ddd955999eedaf251ea44af1dc8
systemis <cuthinh2002@icloud.com>
2017-07-13 09:43:50 +0700

    add mysql in config file

--- server/config/mainDB.js
+++ server/config/mainDB.js
@@ -2,10 +2,20 @@ var mysql = require('mysql');
 
 module.exports = mysql.createConnection({
     connectionLimit: 100,
-    host: 'localhost',
-    port: 8889,
-    user: 'root',
-    password: 'root',
-    database: 'TruongThinhECommerce',
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    host: 'db4free.net',
+    port: 3306,
+    user: 'tt_database_tt',
+    password: 'Technology12',
+    database: 'tt_database_tt',
+    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
+
+// module.exports = mysql.createConnection({
+//     connectionLimit: 100,
+//     host: 'localhost',
+//     port: 8889,
+//     user: 'root',
+//     password: 'root',
+//     database: 'tt_database_tt',
+//     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+// })
